b9c0cc4b2c258ac0728216412df93c2674e5627c
ionasloan <66360926+ionasloan@users.noreply.github.com>
2022-12-15 16:04:36 +0000

    Logo/Nav - working

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './App.css';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+    <Router>
+    <Navbar />
+         <Routes>
+            <Route path='/' exact />
+        </Routes>
+    </Router>
+    </>
+
   );
 }
 
--- project/src/components/navbar.js
+++ project/src/components/navbar.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
-
-function navbar() {
+function Navbar() {
+   
   return (
     <>
        <nav className='navbar'>
         <div className='navbar-container'>
-
-
-
+            <Link to="/" className="navbar-logo">
+                Glasgow Film Festival
+                <i className='fab fa-typo3'/>
+            </Link>   
+            
         </div>
        </nav>
     </>
   )
 }
 
-export default navbar
+export default Navbar
