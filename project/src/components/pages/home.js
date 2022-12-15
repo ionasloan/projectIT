bdf7eb619afc3ac8ed15eb3854dfdf9239dd87fc
ionasloan <66360926+ionasloan@users.noreply.github.com>
2022-12-15 21:35:59 +0000

    Footer - working

--- project/src/components/pages/home.js
+++ project/src/components/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../App.css';
 import Main from '../main';
-
+import Footer from '../footer';
 import Card from '../card';
 
 function Home () {
@@ -9,6 +9,7 @@ function Home () {
         <>
         <Main />
         <Card />
+        <Footer />
         </>
     );
 }
