cee8d3ec1ffc259e854da6ba85dabb569b8b36eb
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 14:46:24 +0000

    modified:   project/src/components/pages/login.js

--- project/src/components/pages/login.js
+++ project/src/components/pages/login.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import '../../App.css';
 
@@ -7,3 +8,92 @@ export default function Login () {
   )
 }
 
+*/
+
+import React from 'react';
+import '../../App.css';
+
+class Login extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: ""
+    };
+  }
+
+  changeInputValue(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  validationForm() {
+    let returnData = {
+      error : false,
+      msg: ''
+    }
+    const {password} = this.state
+    //Check password
+    if(password.length < 8) {
+      returnData = {
+        error: true,
+        msg: 'Password must be more than 8 characters'
+      }
+    }
+    return returnData;
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    const validation = this.validationForm()
+    var username = e.target.elements.username.value;
+    var password = e.target.elements.password.value;
+    if (validation.error) {
+      alert(validation.msg)
+    }else if(username === 'admin1' && password === '12345678') {
+      alert("Login successful");
+    }else {
+      alert("Wrong password or username");
+    }
+  }
+    
+  render() {
+    return (
+      <div className="container" style={{ paddingTop: "5%" }}>
+        <form
+          onSubmit={e => {
+            this.submitForm(e);
+          }}
+        >
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              placeholder="Username"
+              onChange={e => this.changeInputValue(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              placeholder="Password"
+              onChange={e => this.changeInputValue(e)}
+              
+            />
+          </div>
+          <button value="submit" className="btn btn-primary" onClick={this.postDetails}>
+            Submit
+          </button>
+        </form>
+      </div>
+    
+    );
+  }
+}
+
+export default Login;
