07b5be01a082344f5c0d8dbd719e0148f6d922e8
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 15:46:34 +0000

    modified:   project/src/components/login.js

--- project/src/components/login.js
+++ project/src/components/login.js
@@ -1,53 +1,6 @@
 import React from 'react';
 
 function Login(){
-    class Login extends React.Component {
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        userName: "",
-        password: ""
-      };
-    }
-  
-    changeInputValue(e) {
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-    }
-  
-    validationForm() {
-      let returnData = {
-        error : false,
-        msg: ''
-      }
-      const {password} = this.state
-      //Check password
-      if(password.length < 8) {
-        returnData = {
-          error: true,
-          msg: 'Password must be more than 8 characters'
-        }
-      }
-      return returnData;
-    }
-  
-    submitForm(e) {
-      e.preventDefault();
-      const validation = this.validationForm()
-      var username = e.target.elements.username.value;
-      var password = e.target.elements.password.value;
-      if (validation.error) {
-        alert(validation.msg)
-      }else if(username === 'iona.sloan@gmail.com' && password === 'password') {
-        window.location = "/";
-      }else {
-        alert("Wrong password or username");
-      }
-    }
-      
-    render() {
       return (
         <div className="container" style={{ paddingTop: "5%" }}>
   
@@ -84,5 +37,3 @@ function Login(){
       
       );
     }
-  }
-}
