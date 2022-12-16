98bcc2161cb132482e1d730771957785090d5658
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 17:37:58 +0000

    modified:   project/src/components/about.js

--- project/src/components/about.js
+++ project/src/components/about.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './about.css';
+import gff from '...images/gff.jpg';
 
 function Aboutus() {
   return (
     <div className='card'>
-        <h1>Our Locations</h1>
-        <h3>Glasgow is one of the friendliest film festivals on the planet with a wide-ranging programme that celebrates every corner of world cinema and provides a fantastic showcase for the best of Scottish film. The 2023 edition will take place from Wednesday 1 to Sunday 12 March from our beloved Glasgow Film Theatre, at selected venues across the city, our online player Glasgow Film At Home (GFAH), and in cinema venues across the UK.</h3>
+        <div>
+            <img src={gff} alt="gff"/>
+        </div>
+        <h1>About</h1>
+        <h3>Glasgow is one of the friendliest film festivals on the planet with a wide-ranging programme 
+            that celebrates every corner of world cinema and provides a fantastic showcase for the best of 
+            Scottish film. The 2023 edition will take place from Wednesday 1 to Sunday 12 March from 
+            our beloved Glasgow Film Theatre, at selected venues across the city, our online player 
+            Glasgow Film At Home (GFAH), and in cinema venues across the UK.</h3>
     
     </div>
   );
