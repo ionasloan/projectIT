9c03ac31fddefedee9775bff24f8e46da6c1ed21
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 19:39:32 +0000

    modified:   project/src/components/manageevents.js 	modified:   project/src/components/pages/adminmanage.js

--- project/src/components/manageevents.js
+++ project/src/components/manageevents.js
@@ -1,45 +1,33 @@
 import React from 'react';
-import './login.css';
+import './about.css';
 
-class ManageEvent extends React.Component {
-    
-  render() {
-    return (
-      <div className="container" style={{ paddingTop: "5%" }}>
+function ManageEvents() {
+  return (
+    <div class="about">
+        <h1>About</h1>
+        <div class="image">
+            <img src="./images/gff.jpg" alt="gff"/>
+        </div>
+        
+        <h2>Glasgow is one of the friendliest film festivals on the planet with a wide-ranging programme 
+            that celebrates every corner of world cinema and provides a fantastic showcase for the best of 
+            Scottish film. The 2023 edition will take place from the 2nd tothe 23rd March from 
+            our beloved Glasgow Film Theatre, at selected venues across the city, our online player 
+            Glasgow Film At Home (GFAH), and in cinema venues across the UK.</h2>
 
-      <h1 className='login'>Log In Here</h1>
-        <form class="form"
-          onSubmit={e => {
-            this.submitForm(e);
-          }}
-        >
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              placeholder="Username"
-              onChange={e => this.changeInputValue(e)}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              placeholder="Password"
-              onChange={e => this.changeInputValue(e)}
-              
-            />
-          </div>
-          <button class="button" value="submit" className="btn btn-primary" onClick={this.postDetails}>
-            Submit
-          </button>
-        </form>
-      </div>
+            <div class="image">
+            <img src="./images/gff2.jpg" alt="gff"/>
+        </div>
+
+        <h3>
+            The Festival has a long tradition of championing new talent and ensuring that the very best
+             films from the annual submissions are given a platform in the public programme. 2022 
+             submissions that screened at the Festival included Lizzie MacKenzie's Audience Award 
+             winning The Hermit of Treig.
+        </h3>
     
-    );
-  }
+    </div>
+  );
 }
 
-export default ManageEvent;
+export default ManageEvents
--- project/src/components/pages/adminmanage.js
+++ project/src/components/pages/adminmanage.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import './about.css';
+import '../../App.css';
+import ManageEvents from '../about';
+import Footer from '../footer';
 
-function Aboutus() {
+
+export default function Manage() {
   return (
-    <div class="about">
-        <h1>About</h1>
-        <div class="image">
-            <img src="./images/gff.jpg" alt="gff"/>
-        </div>
-        
-        <h2>Glasgow is one of the friendliest film festivals on the planet with a wide-ranging programme 
-            that celebrates every corner of world cinema and provides a fantastic showcase for the best of 
-            Scottish film. The 2023 edition will take place from the 2nd tothe 23rd March from 
-            our beloved Glasgow Film Theatre, at selected venues across the city, our online player 
-            Glasgow Film At Home (GFAH), and in cinema venues across the UK.</h2>
-
-            <div class="image">
-            <img src="./images/gff2.jpg" alt="gff"/>
-        </div>
-
-        <h3>
-            The Festival has a long tradition of championing new talent and ensuring that the very best
-             films from the annual submissions are given a platform in the public programme. 2022 
-             submissions that screened at the Festival included Lizzie MacKenzie's Audience Award 
-             winning The Hermit of Treig.
-        </h3>
-    
-    </div>
-  );
+      <>
+      <ManageEvents />
+      <Footer />
+      </>
+  )
 }
-
-export default Aboutus
