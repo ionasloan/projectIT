21f48d65ebbec0b5dc82bf60629ba5ce3d141448
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 15:20:19 +0000

    new file:   project/src/components/Login.js 	modified:   project/src/components/pages/login.js

--- project/src/components/pages/login.js
+++ project/src/components/pages/login.js
@@ -1,17 +1,6 @@
-/*
 import React from 'react';
 import '../../App.css';
-
-export default function Login () {
-  return (
-   <h1 className='login'>Log In Here</h1>
-  )
-}
-
-*/
-
-import React from 'react';
-import '../../App.css';
+import Login from '../Login';
 
 class Login extends React.Component {
   
@@ -57,44 +46,6 @@ class Login extends React.Component {
     }else {
       alert("Wrong password or username");
     }
-  }
-    
-  render() {
-    return (
-      <div className="container" style={{ paddingTop: "5%" }}>
-
-      <h1 className='login'>Log In Here</h1>
-        <form
-          onSubmit={e => {
-            this.submitForm(e);
-          }}
-        >
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              placeholder="Username"
-              onChange={e => this.changeInputValue(e)}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              placeholder="Password"
-              onChange={e => this.changeInputValue(e)}
-              
-            />
-          </div>
-          <button value="submit" className="btn btn-primary" onClick={this.postDetails}>
-            Submit
-          </button>
-        </form>
-      </div>
-    
-    );
   }
 }
 
