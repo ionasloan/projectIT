d2dc3780f07f04a9cc80e91b366b769f5fa3e30b
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 15:32:10 +0000

    deleted:    project/src/components/Login.js 	new file:   project/src/components/login.js 	modified:   project/src/components/pages/login.js

--- project/src/components/pages/login.js
+++ project/src/components/pages/login.js
@@ -1,52 +1,12 @@
 import React from 'react';
 import '../../App.css';
-import Login from '../Login';
+import Login from '../login';
 
-class Login extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: "",
-      password: ""
-    };
-  }
+export default function Login() {
+  return (
+      <>
+      <Login />
 
-  changeInputValue(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-
-  validationForm() {
-    let returnData = {
-      error : false,
-      msg: ''
-    }
-    const {password} = this.state
-    //Check password
-    if(password.length < 8) {
-      returnData = {
-        error: true,
-        msg: 'Password must be more than 8 characters'
-      }
-    }
-    return returnData;
-  }
-
-  submitForm(e) {
-    e.preventDefault();
-    const validation = this.validationForm()
-    var username = e.target.elements.username.value;
-    var password = e.target.elements.password.value;
-    if (validation.error) {
-      alert(validation.msg)
-    }else if(username === 'iona.sloan@gmail.com' && password === 'password') {
-      window.location = "/";
-    }else {
-      alert("Wrong password or username");
-    }
-  }
+      </>
+  )
 }
-
-export default Login;
