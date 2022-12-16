240b45c0f86eebfa10ca4500c2ebaf4ca0da884e
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 19:32:36 +0000

    new file:   project/src/components/manageevents.js 	new file:   project/src/components/pages/adminmanage.js

---
+++ project/src/components/pages/adminmanage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import ManageEvent from '../manageevents';
+import Footer from '../footer';
+
+export default function Admin() {
+  return (
+      <>
+      <ManageEvent />
+      <Footer />
+      </>
+  )
+}
+
