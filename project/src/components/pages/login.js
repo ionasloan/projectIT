ca6ebd3fc3bac59a33896c6f4a58fd66c557a5dc
emzomc <mchugh_emily1997@hotmail.co.uk>
2022-12-16 16:07:35 +0000

    modified:   project/src/components/pages/login.js

--- project/src/components/pages/login.js
+++ project/src/components/pages/login.js
@@ -1,15 +1,3 @@
-/*
-import React from 'react';
-import '../../App.css';
-
-export default function Login () {
-  return (
-   <h1 className='login'>Log In Here</h1>
-  )
-}
-
-*/
-
 import React from 'react';
 import '../../App.css';
 
